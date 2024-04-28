e20496137b77658dd6518fa2acd250b2ba420bbd
Marcos <m.vinicius.sr11@gmail.com>
2024-04-28 15:31:40 -0300

    importando TabelaHash

---
+++ src/Teste.java
@@ -0,0 +1,12 @@
+import TabelaHash.TabelaHash;
+import dados.Pessoa;
+import listaEncadeada.ListaEncadeada;
+
+public class Teste{
+
+    public static void main(String[] args) {
+        ListaEncadeada<Pessoa> lista = new ListaEncadeada<>();
+    }
+
+
+}
